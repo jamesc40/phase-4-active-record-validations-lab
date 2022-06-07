9f27428afd61db71598a5ebf02536cc93442551d
James Coffman <jamesc4023@gmail.com>
2022-06-07 12:42:12 -0400

    commit tree 1f60c9f4a2f639a87359674a06fa251276d57f02

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 } 
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :check_for_clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def check_for_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |string| string.match title }
+      errors.add(:title, 'must be clickbait')
+    end
+  end
 end
